c028f8abb3716523d1b9c38df96df1f8ef92986f
SebastisaanVDW <asr.vanderweel@gmail.com>
2019-01-14 14:03:26 +0100

    Dit is progress

---
+++ app/src/main/java/vdw/asr/blijdorpapp/MainActivity.java
@@ -0,0 +1,22 @@
+package vdw.asr.blijdorpapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_INFO = "default";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
